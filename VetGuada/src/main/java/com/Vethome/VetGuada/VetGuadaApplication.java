c1a2c1ff99211fd824e07d5d422db341ec51e366
SilvestreNovaro <silvestrenovarohueyo@gmail.com>
2023-09-25 16:32:22 -0300

    first commit

---
+++ VetGuada/src/main/java/com/Vethome/VetGuada/VetGuadaApplication.java
@@ -0,0 +1,13 @@
+package com.Vethome.VetGuada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetGuadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetGuadaApplication.class, args);
+	}
+
+}
---
+++ VetGuada/src/test/java/com/Vethome/VetGuada/VetGuadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Vethome.VetGuada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetGuadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
